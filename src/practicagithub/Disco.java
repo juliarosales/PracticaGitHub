ae84f759d244d66824e8d6bf329b0137683eaf6c
Luzmar <luzmar115@hotmail.com>
2012-11-09 15:37:31 -0430

    Se agrega funcion de buscardisco

--- src/practicagithub/Disco.java
+++ src/practicagithub/Disco.java
@@ -46,47 +46,33 @@ public class Disco extends Obra
     //Funcion para imprimir
     public void imprimir()
     {
-         System.out.println("\t Título: "+this.getTitulo());
-         System.out.println("\t Autor: "+this.getAutor());
-         System.out.println("\t A#o Edición: "+this.getAnoEdicion());
+         super.mostrarDatos();
          System.out.println("\t Discografia: "+this.getDiscografia());
          System.out.println("\t Número de Canciones: "+this.getNCanciones());         
 
     }
-    
-    public void cargardatos() throws IOException
+  
+    // Busqueda de discos
+    public List<Disco> buscardisco (List<Disco> discos, String busqueda)
     {
-       
-       InputStreamReader entrada = new InputStreamReader(System.in);
-       BufferedReader buf = new BufferedReader(entrada);
-       this.interpretes = new ArrayList<>();
-       
-       while(this.resp.equals("S") || this.resp.equals("s")){
-       
-            System.out.print("Ingrese el Título: ");
-            this.setTitulo(buf.readLine());
-            System.out.print("Ingrese el Autor: ");
-            //nom = buf.readLine();
-            System.out.print("Ingrese el A#o de Edicion: ");
-            try {
-                this.setAnoEdicion( Integer.parseInt(buf.readLine()));
-            } catch (NumberFormatException ex) {
-            System.out.println("  Error el A#o de Edicion, intentelo de nuevo.");
+        List<Disco> resultados = new ArrayList<>();
+        for (Disco disco : discos) 
+        {
+            if (disco.getTitulo().compareToIgnoreCase(busqueda) == 0 )
+            {
+                resultados.add(disco);
             }
-            System.out.print("Ingrese el Discografia: ");
-            this.setDiscografia(buf.readLine());
-            System.out.print("Ingrese el Numero de Canciones: ");
-            try {
-                this.setNCanciones( Integer.parseInt(buf.readLine()));
-            } catch (NumberFormatException ex) {
-            System.out.println("  Error el Numero de Canciones, intentelo de nuevo.");
+        }
+        
+        for (Disco disco : discos) 
+        {
+            if (disco.getAutor().getNombre().compareToIgnoreCase(busqueda) == 0 )
+            {
+                resultados.add(disco);
             }
-
-            System.out.println("Desea agregar otro Disco? S/N");
-            resp = buf.readLine();
-       } 
-       
-       
+        }
+      
+        return resultados;
     }
     
 }
